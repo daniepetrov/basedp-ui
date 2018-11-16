09711ed9ffe514d8e57154d968c221f74bb1f1c1
dapetrov0 <dapetrov0@gmail.com>
2018-11-16 18:41:00 +0300

    Fixes

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'LandingContext'
-import { Box } from './../../src'
+// import { Box } from './../../src'
+import { Box } from './../../dist'
 
 const App = () => (
   <Provider value={{ locale: 'RU' }}>
